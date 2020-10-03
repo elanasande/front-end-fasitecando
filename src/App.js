39720a93c3abc0ccb4826348f3a41aadb1cde7ad
elanasande <elanatanan@gmail.com>
2020-10-03 14:15:24 -0300

    :construction: estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
