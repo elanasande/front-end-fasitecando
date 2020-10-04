72aefa140555f0eae80056effbd0270bf44fe1d5
elanasande <elanatanan@gmail.com>
2020-10-03 22:13:37 -0300

    feat(login): Componentes button e input

---
+++ src/Components/Forms/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = ({ label }) => {
+  return (
+    <React.Fragment>
+      <label>{label}</label>
+      <input></input>
+    </React.Fragment>
+  );
+};
+
+export default Input;
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Input label="Nome"></Input>
+      <Input label="Senha"></Input>
+      <Button>Entrar</Button>
+    </div>
+  );
 };
 
 export default Login;
